5224d8b94a990862522212f4e694650e68b604e2
octopse <octopse.octopse@gmail.com>
2022-12-20 19:37:53 +0100

    Added hover to nav bar text

--- components/NavBarLarge.jsx
+++ components/NavBarLarge.jsx
@@ -8,16 +8,16 @@ function NavBarLarge(){
         <div className="w-4/5 py-4 mx-auto flex justify-between items-center ">
             <Image src={logo} />
             <div className="flex items-center space-x-4 text-lg ">
-                <a href="#">Events</a>
-                <a href="#">Forum</a>
-                <a href="#">Articles</a>
-                <a href="#">Podcasts</a>
-                <a href="#">Jobs</a>
-                <div className="flex items-center space-x-6 text-base font-medium">
-                    <div className="flex justify-center items-center bg-brand-light opacity-8 p-2 rounded-full border-2 border-green-100">
+                <a href="#" className="hover:text-brand">Events</a>
+                <a href="#" className="hover:text-brand">Forum</a>
+                <a href="#" className="hover:text-brand">Articles</a>
+                <a href="#" className="hover:text-brand">Podcasts</a>
+                <a href="#" className="hover:text-brand">Jobs</a>
+                <div className="flex items-center text-base font-medium">
+                    <div className="flex justify-center items-center mr-6 bg-brand-light opacity-8 p-2 rounded-full border-2 border-green-100">
                         <Image src={searchIcon} />
                     </div>
-                    <a href="#" className="p-2 px-4 bg-brand text-white rounded-md hover:opacity-90">Login</a>
+                    <a href="#" className="p-2 px-4 mr-4 bg-brand text-white rounded-md hover:opacity-90">Login</a>
                     <a href="#" className="text-brand p-2 px-4 hover:border-2 hover:border-brand hover:bg-brand-light hover:rounded-md">Sign up</a>
                 </div>
             </div>
