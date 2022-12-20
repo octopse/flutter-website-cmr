443d0a9362d4985f17a7af87f9605b50c7db8380
octopse <octopse.octopse@gmail.com>
2022-12-20 18:57:12 +0100

    Updated nav bar font size, nav bar font weight and nav bar line separation

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -2,7 +2,7 @@ import NavBarLarge from "./NavBarLarge"
 
 function NavBar(){
     return( 
-        <nav className="w-full border-b-2 border-gray-300">
+        <nav className="w-full border-b-2 border-gray-100">
             <NavBarLarge />
         </nav>
     )
--- components/NavBarLarge.jsx
+++ components/NavBarLarge.jsx
@@ -7,18 +7,18 @@ function NavBarLarge(){
     return(
         <div className="w-4/5 py-4 mx-auto flex justify-between items-center ">
             <Image src={logo} />
-            <div className="flex items-center space-x-4 text-xl ">
+            <div className="flex items-center space-x-4 text-lg ">
                 <a href="#">Events</a>
                 <a href="#">Forum</a>
                 <a href="#">Articles</a>
                 <a href="#">Podcasts</a>
                 <a href="#">Jobs</a>
-                <div className="flex items-center space-x-6 text-base">
+                <div className="flex items-center space-x-6 text-base font-medium">
                     <div className="flex justify-center items-center bg-green-100 p-2 rounded-full border-2 border-green-200">
                         <Image src={searchIcon} />
                     </div>
                     <a href="#" className="p-2 px-4 bg-green-900 text-white rounded-md">Login</a>
-                    <a href="#" className="text-green-900">Signup</a>
+                    <a href="#" className="text-green-900 ">Sign up</a>
                 </div>
             </div>
         </div>
