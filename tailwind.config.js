2e9389c6fe775d844b5acb2ae3702163fc6fa3d4
octopse <octopse.octopse@gmail.com>
2022-12-20 19:08:52 +0100

    Added brand colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,13 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: {
+          DEFAULT: "#007a5e",
+        }
+      }
+    },
   },
   plugins: [],
 }
