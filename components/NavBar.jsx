57f6f81ba75e3ded7a9f69fcce73d3616c79ab4e
octopse <octopse.octopse@gmail.com>
2022-12-22 14:01:26 +0100

    Updated Nav-bar; now position fixed

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -2,7 +2,7 @@ import NavBarLarge from "./NavBarLarge"
 
 function NavBar(){
     return( 
-        <nav className="w-full border-b-2 border-gray-100">
+        <nav className="w-full border-b-2 border-gray-100 fixed r-o t-0 z-50 bg-white">
             <NavBarLarge />
         </nav>
     )
