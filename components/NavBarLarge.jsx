d94f4551694d142a21d67a20cb4f44c33e53a927
octopse <octopse.octopse@gmail.com>
2022-12-22 04:02:33 +0100

    Added vertically seperation line in nav bar

--- components/NavBarLarge.jsx
+++ components/NavBarLarge.jsx
@@ -7,18 +7,18 @@ function NavBarLarge(){
     return(
         <div className="w-4/5 py-4 mx-auto flex justify-between items-center ">
             <Image src={logo} />
-            <div className="flex items-center space-x-4 text-lg ">
+            <div className="flex items-center space-x-4 text-lg font-medium">
                 <a href="#" className="hover:text-brand">Events</a>
                 <a href="#" className="hover:text-brand">Forum</a>
                 <a href="#" className="hover:text-brand">Articles</a>
                 <a href="#" className="hover:text-brand">Podcasts</a>
                 <a href="#" className="hover:text-brand">Jobs</a>
-                <div className="flex items-center text-base font-medium">
-                    <div className="flex justify-center items-center mr-6 bg-brand-light opacity-8 p-2 rounded-full border-2 border-green-100">
+                <div className="flex items-center border-l-2 border-gray-200 pl-4 text-base">
+                    <div className="flex justify-center items-center mr-8 bg-brand-light opacity-8 p-2 rounded-full border-2 border-green-100 cursor-pointer">
                         <Image src={searchIcon} />
                     </div>
                     <a href="#" className="p-2 px-4 mr-4 bg-brand text-white rounded-md hover:opacity-90">Login</a>
-                    <a href="#" className="text-brand p-2 px-4 hover:border-2 hover:border-brand hover:bg-brand-light hover:rounded-md">Sign up</a>
+                    <a href="#" className="text-brand py-2 hover:px-4 hover:border-2 hover:border-brand hover:rounded-md">Sign up</a>
                 </div>
             </div>
         </div>
