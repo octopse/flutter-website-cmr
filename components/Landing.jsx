90f1d4aeb89c79e16f650ee6700b4f73a39263c1
octopse <octopse.octopse@gmail.com>
2022-12-22 13:27:11 +0100

    Added right side of landing page

--- components/Landing.jsx
+++ components/Landing.jsx
@@ -1,9 +1,15 @@
 import Link from 'next/Link'
+import Image from 'next/image'
 import LandingStat from "./LandingStat"
+
+import img1 from '../public/images/1.jpeg'
+import img2 from '../public/images/2.jpeg'
+import img3 from '../public/images/3.jpeg'
+import img4 from '../public/images/4.jpeg'
 
 function Landing(){
     return(
-        <section>
+        <section className="flex">
             {/* Left side */}
             <div className="w-1/2 space-y-4">
                 <h1 className="text-6xl font-bold">A commuinty open to everyone</h1>
@@ -18,15 +24,28 @@ function Landing(){
                         <a className="border-2 border-brand text-brand font-medium p-2 rounded-md">Visit forum</a>
                     </Link>
                 </div>
-            </div>
-            <div className="space-x-4 flex">
+                <div className="space-x-4 flex">
                 <LandingStat number="106k" title="Members on Meetup" />
                 <LandingStat number="127k" title="Stars on Github" />
                 <LandingStat number="151.4k" title="Followers on Twitter" />
+                </div>
             </div>
             {/* Right side */}
-            <div>
-                
+            <div className="w-1/2 flex items-center justify-end">
+                <div className="w-[30%] h-[500px] relative rounded-full overflow-hidden">
+                    <Image src={img1} alt="image 1" layout="fill" objectFit="cover"/>
+                </div>
+                <div className="w-[30%] h-[1000px]">
+                    <div className="w-full h-1/2 relative rounded-full overflow-hidden">
+                        <Image src={img2} alt="image 2" layout="fill" objectFit="cover" />
+                    </div>
+                    <div className="w-full h-1/2 relative rounded-full overflow-hidden">
+                        <Image src={img3} alt="image 3" layout="fill" objectFit="cover" />
+                    </div>
+                </div>
+                <div className="w-[30%] h-[500px] relative rounded-full overflow-hidden">
+                    <Image src={img4} alt="image 4" layout="fill" objectFit="cover" />
+                </div>
             </div>
         </section>
     )
