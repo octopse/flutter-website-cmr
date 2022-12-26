5afb6e528afaac71a822af7d6cc240a43ab1c359
octopse <octopse.octopse@gmail.com>
2022-12-26 13:24:39 +0100

    Added articles section hover elements

--- components/ArticlesCard.jsx
+++ components/ArticlesCard.jsx
@@ -4,6 +4,32 @@ function ArticlesCard({title, image}){
     return(
         <div className="p-4 w-full h-full bg-no-repeat bg-left-top bg-cover rounded-xl" style={{backgroundImage: `url(${image.src})`}}>
             <h3 className="text-white font-semibold">{title}</h3>
+            <div className="h-[200px] w-full bg-white p-4 rounded-xl space-y-2">
+                <div className="flex mt-2 space-x-4">
+                    <div className="px-2 flex items-center bg-brand-yellow rounded-md">
+                        <p className="w-2 h-2 mr-1 bg-black rounded-full"> </p>
+                        <p className="text-sm">widget</p>
+                    </div>
+                    <div className="px-2 flex items-center bg-brand-yellow rounded-md">
+                        <p className="w-2 h-2 mr-1 bg-brand rounded-full"> </p>
+                        <p className="text-sm">features</p>
+                    </div>
+                </div>
+                <div>
+                    <h4 className="font-medium text-lg">Introducing Flutter in Focus!</h4>
+                    <p className="opacity-60 text-sm">In Flutter, "everything is a widget!"
+                        If you want to better understand the two kinds fo widget,
+                        Stateless and Statefull, see the following videos. </p>
+                </div>
+                <div className="flex justify-between items-center">
+                    <p className="text-sm opacity-40">03 August 2022 - 02 minutes</p>
+                    <div className="flex items-center">
+                        <p className="mr-2 w-6 h-6 rounded-full bg-gray-400"> </p>
+                        <p className="opacity-60 text-sm">Boris Kamtou</p>
+                    </div>
+                </div>
+            </div>
+
         </div>
     )
 }
