cdddfe848ae506ffd9ec1d3836355c0991491894
octopse <octopse.octopse@gmail.com>
2022-12-28 15:09:16 +0100

    Update: Footer styled

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,10 +1,18 @@
+import Image from 'next/image'
 
+import imgFooter1 from '../public/images/12.svg'
+import imgFooter2 from '../public/images/25.svg'
+import imgFooter3 from '../public/images/27.svg'
+import imgFooter4 from '../public/images/10.svg'
+import imgFooter5 from '../public/images/26.svg'
+import imgFooter6 from '../public/images/28.svg'
+import imgFooter7 from '../public/images/30.svg'
 
 
 function Footer(){
     return(
         <footer className="w-4/5 mx-auto mt-8">
-            <div className="flex justify-between p-4">
+            <div className="flex justify-between p-4 bg-gray-100 rounded-2xl">
                 <div className="w-1/4">
                     <h3 className="text-2xl font-semibold mb-4">Resources</h3>
                     <p>Become a Sponsor</p>
@@ -13,10 +21,16 @@ function Footer(){
                     <p>Podcasts</p>
                     <p>Jobs</p>
                     <p>Branding</p>
-                    <p>#DROP DOWN MENU#</p>
-                    <div>
-                        <span>Slack</span>
-                        <span>Telegram</span>
+                    <div className="bg-black flex space-x-2 rounded-md p-2 mt-2 w-fit cursor-pointer"><img src={imgFooter1.src} className="w-auto h-6" /><span className="text-white"> English</span></div>
+                    <div className="flex space-x-2 mt-4">
+                       <div className="flex items-center font-medium space-x-2 bg-white w-fit p-2 rounded-md cursor-pointer border-2 border-gray-100 hover:border-black">
+                            <Image src={imgFooter2} />
+                            <span className="">Slack</span>
+                        </div>
+                        <div className="flex items-center font-medium space-x-2 bg-white w-fit p-2 rounded-md cursor-pointer border-2 border-gray-100 hover:border-black">
+                            <Image src={imgFooter3} />
+                            <span>Telegram</span>
+                        </div>
                     </div>
                 </div>
                 <div className="w-1/4">
@@ -41,11 +55,16 @@ function Footer(){
                 </div>
             </div>
             <hr className="mt-4 w-full border border-gray-200" />
-            <div className="mt-4 flex justify-between">
+            <div className="mt-4 flex justify-between mb-4">
                 <p>&copy; 2022 - 2023 Flutter Cameroun. All rights reserve Design by
                     <a href="#" className="text-brand"> MBAGNA Johan</a> (some adjustment by me)
                 </p>
-                <p>#icons#</p>
+                <div className='space-x-2'>
+                    <a href="#"><Image src={imgFooter4}/></a>
+                    <a href="#"><Image src={imgFooter5}/></a>
+                    <a href="#"><Image src={imgFooter6}/></a>
+                    <a href="#"><Image src={imgFooter7}/></a>
+                </div>
             </div>
         </footer>
     )
