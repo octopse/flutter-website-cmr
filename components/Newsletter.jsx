b7dbfdcc83234e22f29fcdb94c25602b7921876c
octopse <octopse.octopse@gmail.com>
2022-12-28 16:04:19 +0100

    Adjusted newsletter input display and on edit display

--- components/Newsletter.jsx
+++ components/Newsletter.jsx
@@ -8,7 +8,7 @@ function Newsletter(){
                 <h2 className="text-5xl">Join our newsletter</h2>
                 <p className="font-light opacity-80">Join our newsletter to receive tutorials, article and podcasts on web design and programming.</p>
                 <div className="flex w-full space-x-2 px-16 mt-4">
-                    <input type="text" placeholder="Enter your email address" className="p-2 w-5/6 rounded-md"/>
+                    <input type="text" placeholder="Enter your email address" className="p-2 w-5/6 rounded-md outline-none text-gray-600"/>
                     <input type="submit" value="Sign up" className="bg-green-500 text-brand rounded-md p-2 w-1/6 cursor-pointer hover:bg-none hover:border-2 hover:border-green-500" />
                 </div>
             </form>
