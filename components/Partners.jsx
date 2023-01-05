ed5b037eb46ed0eb2c99069b006d19e3cd04fd0e
octopse <octopse.octopse@gmail.com>
2023-01-05 17:05:05 +0100

    Responsive partner-section

--- components/Partners.jsx
+++ components/Partners.jsx
@@ -10,27 +10,30 @@ import imgPartner6 from '../public/images/datk.png'
 
 function Partners(){
     return(
-        <section className="mt-12 w-4/5 mx-auto p-8 bg-black text-white rounded-xl flex">
-            <div className="w-3/4 pr-4">
+        <section className="mt-24 w-11/12 mx-auto p-4 lg:p-8 bg-black text-white rounded-3xl flex flex-col
+            lg:w-4/5 lg:flex-row lg:mt-12">
+            <div className="w-full lg:pr-4 lg:w-3/4">
                 <h2 className="text-3xl font-semibold">Partners</h2>
                 <p className="opacity-80 mb-8">A very big thank you to all our partners for their continued partnership.
                     If you are interested in being showcased throughout Flutter Cameroon, 
                     contact <span className="text-brand-yellow">sponsor@fluttercm.dev</span> to discuss sponshorship opportunities.
                 </p>
                 <Link href="#">
-                    <a className="p-2 border-2 text-brand-yellow border-brand-yellow rounded-md hover:bg-brand-yellow hover:text-black">Become a partner</a>
+                    <p className="flex justify-center p-2 text-brand-yellow border-2 border-brand-yellow rounded-md cursor-pointer
+                        hover:bg-brand-yellow hover:text-black
+                        xsm:w-fit">Become a partner</p>
                 </Link>
             </div>
-            <div className="w-1/4 flex flex-col justify-between">  
-                <div className="flex justify-between">
+            <div className="flex flex-wrap justify-center gap-x-4 lg:w-1/4 lg:flex-col lg:justify-between ">  
+                <div className="flex justify-between gap-x-4">
                     <Image src={imgPartner1} alt="logo like concept" />
                     <Image src={imgPartner2} alt="logo laravel cameroon" />
                 </div>
-                <div className="flex justify-around -mt-4">
+                <div className="flex justify-around lg:-mt-4 gap-x-4">
                     <Image src={imgPartner3}/>
                     <Image src={imgPartner4}/>
                 </div>
-                <div className="flex justify-between mt-2">
+                <div className="flex justify-between items-center lg:mt-2 gap-x-4">
                     <a href="https:www.instagram.com/datk.creative"><img src={imgPartner6.src} className="w-auto h-8" alt="logo datk Creative. Instagram and Facebook @datk.creative" /></a>
                     {/* <div className="h-20 w-auto bg-blue-800 relative overflow-hidden">
                     </div> */}
