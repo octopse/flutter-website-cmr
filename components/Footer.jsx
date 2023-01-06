4a77aff92c44a53f56cb7c7147e8c51e0e73d78b
octopse <octopse.octopse@gmail.com>
2023-01-06 20:10:41 +0100

    Responsive Footer-section

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -11,9 +11,11 @@ import imgFooter7 from '../public/images/30.svg'
 
 function Footer(){
     return(
-        <footer className="w-4/5 mx-auto mt-8">
-            <div className="flex justify-between p-4 bg-gray-100 rounded-2xl">
-                <div className="w-1/4">
+        <footer className="w-11/12 mx-auto mt-8 lg:w-4/5 ">
+            <div className="flex flex-col p-4 gap-x-4 gap-y-4 bg-gray-100 rounded-2xl rounded-b-none
+                xsm:flex-row xsm:flex-wrap xsm:justify-between
+                md:flex-nowrap">
+                <div className="md:w-1/4">
                     <h3 className="text-2xl font-semibold mb-4">Resources</h3>
                     <p className="opacity-80 cursor-pointer">Become a Sponsor</p>
                     <p className="opacity-80 cursor-pointer hover:opacity-100">Call for Speakers</p>
@@ -22,44 +24,50 @@ function Footer(){
                     <p className="opacity-80 cursor-pointer hover:opacity-100">Jobs</p>
                     <p className="opacity-80 cursor-pointer hover:opacity-100">Branding</p>
                     <div className="bg-black flex space-x-2 rounded-md p-2 mt-2 w-fit cursor-pointer"><img src={imgFooter1.src} alt="Flag icon" className="w-auto h-6" /><span className="text-white"> English</span></div>
-                    <div className="flex space-x-2 mt-4">
-                       <div className="flex items-center font-medium space-x-2 bg-white w-fit p-2 rounded-md cursor-pointer border-2 border-gray-100 hover:border-brand">
-                            <Image src={imgFooter2} alt="Slack logo" />
-                            <span>Slack</span>
-                        </div>
-                        <div className="flex items-center font-medium space-x-2 bg-white w-fit p-2 rounded-md cursor-pointer border-2 border-gray-100 hover:border-brand">
-                            <Image src={imgFooter3} alt="Telegram logo" />
-                            <span>Telegram</span>
-                        </div>
-                    </div>
                 </div>
-                <div className="w-1/4">
+                <div className="md:w-1/4">
                     <h3 className="text-2xl font-semibold mb-4">Learn Flutter</h3>
                     <p className="opacity-80 cursor-pointer hover:opacity-100">Learning Journey</p>
                     <p className="opacity-80 cursor-pointer hover:opacity-100">codelabs</p>
                     <p className="opacity-80 cursor-pointer hover:opacity-100">Samples</p>
                 </div>
-                <div className="w-1/4">
+                <div className="md:w-1/4">
                     <h3 className="text-2xl font-semibold mb-4">Legal</h3>
                     <p className="opacity-80 cursor-pointer hover:opacity-100">Terms and services</p>
                     <p className="opacity-80 cursor-pointer hover:opacity-100">Code of conduct</p>
                     <p className="opacity-80 cursor-pointer hover:opacity-100">FAQ</p>
                 </div>
-                <div className="w-1/4">
+                <div className="md:w-1/4">
                     <h3 className="text-2xl font-semibold mb-4">Join Us</h3>
                     <p className="opacity-80">Join our newsletter to receive tutorials, articles and podcasts on web design and programming.</p>
-                    <form className="flex w-full mt-4 space-x-2">
-                        <input type="text" placeholder="Your email address" className="p-2 rounded-md border-2 border-gray-200 outline-none"/>
-                        <input type="submit" value="Sign up" className="p-2 bg-brand text-white rounded-md cursor-pointer hover:bg-none hover:opacity-80" />
+                    <form className="flex flex-col w-full mt-4 gap-x-2 gap-y-2
+                        xsm:flex-row">
+                        <input type="text" placeholder="Your email address" className="p-2 w-full rounded-md border-2 border-gray-200 outline-none
+                                                                                xsm:w-[calc(100%-80px)]"/>
+                        <input type="submit" value="Sign up" className="p-2 w-full bg-brand text-white rounded-md cursor-pointer hover:bg-none hover:opacity-80
+                                                                                xsm:w-20" />
                     </form>
                 </div>
             </div>
+            <div className="flex flex-col gap-x-2 gap-y-2 bg-gray-100 rounded-b-2xl px-4 pb-4
+                xsm:flex-row">
+                <div className="w-full p-2 flex items-center justify-center font-medium space-x-2 bg-white rounded-md cursor-pointer border-2 border-gray-100 hover:border-brand
+                    md:w-fit">
+                    <Image src={imgFooter2} alt="Slack logo"/>
+                    <span>Slack</span>
+                </div>
+                <div className="w-full p-2 flex items-center justify-center font-medium space-x-2 bg-white rounded-md cursor-pointer border-2 border-gray-100 hover:border-brand
+                    md:w-fit">
+                    <Image src={imgFooter3} alt="Telegram logo" />
+                    <span>Telegram</span>
+                </div>
+            </div>
             <hr className="mt-4 w-full border border-gray-200" />
-            <div className="mt-4 flex justify-between mb-4">
-                <p className="font-medium">&copy; 2022 - 2023 Flutter Cameroun. All rights reserve Design by
+            <div className="mt-4 flex flex-col text-center md:justify-between mb-4 md:flex-row">
+                <p className="font-medium">&copy; 2022-2023 Flutter Cameroun. All rights reserve Design by
                     <a href="#" className="hover:text-brand"> MBAGNA Johan</a>
                 </p>
-                <div className='space-x-2'>
+                <div className='space-x-2 mt-2 md:mt-0'>
                     <a href="#"><Image src={imgFooter4} alt="Github icon" /></a>
                     <a href="#"><Image src={imgFooter5} alt="Telegram icon" /></a>
                     <a href="#"><Image src={imgFooter6} alt="twitter icon" /></a>
