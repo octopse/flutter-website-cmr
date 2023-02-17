a51e6b09b4e5e4b985f5266ef8b1020a1c4d7b89
octopse <octopse.octopse@gmail.com>
2023-02-17 17:32:35 +0100

    Decreased z-index value

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -3,7 +3,7 @@ import NavBarMobile from "./NavBarMobile"
 
 function NavBar(){
     return( 
-        <nav className="w-full border-b-2 border-gray-100 fixed top-0 left-0 z-50 bg-white max-w-[1600px]">
+        <nav className="w-full border-b-2 border-gray-100 fixed top-0 left-0 z-40 bg-white max-w-[1600px]">
             <NavBarLarge />
             <NavBarMobile />
         </nav>
